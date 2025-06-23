cb9eac64eba0d1eefab358445e32659bb8f79e7a
sachin birajadar <99120546+Birajadarsachin@users.noreply.github.com>
2025-06-24 03:57:42 +0530

    building apis

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import productRoutes from './routes/product.route.js';
+
 dotenv.config();
 
 const app=express();
 
-app.get("/products",(req,res)=>{
-    res.send("Server is ready123");
-});
+const PORT = process.env.PORT || 5000
+
+app.use(express.json());
+//allows to accept JSON data in the req.body
+
+app.use("/api/products",productRoutes);
 
 
-app.listen(5000, ()=>{
+
+
+app.listen(PORT, ()=>{
     connectDB();
-    console.log("server started at http://localhost:5000 ");
+    console.log("server started at http://localhost:"+ PORT);
 });
